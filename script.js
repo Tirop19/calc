16d12822b88cb0126e44e85060b72d9e736d13d2
Tirop19 <tiropadrian89@gmail.com>
2024-03-13 10:03:15 +0300

    calc

---
+++ script.js
@@ -0,0 +1,21 @@
+let currentDisplay ="0";
+
+document.getElementById("button")
+function appendNumber(number) {
+    expression +=number;
+    updateDisplay()
+    
+}
+
+function appendNumber(operator) {
+    expression +=operator;
+    updateDisplay()
+    
+}
+
+function calculate() {
+    const result =eval(expression)
+}
+
+
+
